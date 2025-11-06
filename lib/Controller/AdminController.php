c68a561338335ca3b142467f5d89d0607fd1e211
Kevin Smith <vndinmchine@gmail.com>
2025-11-06 06:36:56 -0500

    Add AdminController for file list management

---
+++ lib/Controller/AdminController.php
@@ -0,0 +1,24 @@
+// ========================
+// lib/Controller/AdminController.php
+// ========================
+
+
+<?php
+namespace OCA\FileListFinder\Controller;
+
+
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
+
+
+class AdminController extends Controller {
+public function __construct($AppName, IRequest $request) {
+parent::__construct($AppName, $request);
+}
+
+
+public function settings(): TemplateResponse {
+return new TemplateResponse('filelistfinder', 'admin');
+}
+}
