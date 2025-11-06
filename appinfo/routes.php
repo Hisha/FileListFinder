b6b74c632a27fb1939195a5578d59d9a37009f2b
Kevin Smith <vndinmchine@gmail.com>
2025-11-06 06:34:49 -0500

    Add routes configuration for User and Admin controllers

---
+++ appinfo/routes.php
@@ -0,0 +1,13 @@
+// ========================
+// appinfo/routes.php
+// ========================
+
+
+<?php
+return [
+'routes' => [
+['name' => 'UserController#index', 'url' => '/user', 'verb' => 'GET'],
+['name' => 'UserController#submit', 'url' => '/user/submit', 'verb' => 'POST'],
+['name' => 'AdminController#settings', 'url' => '/admin', 'verb' => 'GET'],
+]
+];
