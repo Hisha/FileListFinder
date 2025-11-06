52a3ecc0e1ff27c10afb6441e64d731e0fa1c55b
Kevin Smith <vndinmchine@gmail.com>
2025-11-06 06:38:40 -0500

    Create admin.php

---
+++ templates/admin.php
@@ -0,0 +1,7 @@
+// ========================
+// templates/admin.php
+// ========================
+
+
+<h2>Admin Settings</h2>
+<p>This app is currently a skeleton. Groupfolder selection will go here.</p>
