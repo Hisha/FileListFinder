564c8d647b1655e2e88ebd54fdf7a3cbe2778c94
Kevin Smith <vndinmchine@gmail.com>
2025-11-06 07:02:27 -0500

    Create Application class for FileListFinder app

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,25 @@
+<?php
+namespace OCA\FileListFinder\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\BackgroundJob\IJobList;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCA\FileListFinder\BackgroundJob\FileListProcessor;
+
+class Application extends App implements IBootstrap {
+
+    public function __construct(array $urlParams = []) {
+        parent::__construct('filelistfinder', $urlParams);
+    }
+
+    public function register(IRegistrationContext $context): void {
+        // Future: register services, event listeners
+    }
+
+    public function boot(IBootContext $context): void {
+        $jobList = $context->getAppContainer()->get(IJobList::class);
+        $jobList->add(FileListProcessor::class);
+    }
+}
