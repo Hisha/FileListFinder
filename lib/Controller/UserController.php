13181700186d751200d1b678be616ac19fee1d2c
Kevin Smith <vndinmchine@gmail.com>
2025-11-06 06:36:20 -0500

    Create UserController for file list management
    Implement UserController for handling user interactions.

---
+++ lib/Controller/UserController.php
@@ -0,0 +1,38 @@
+// ========================
+// lib/Controller/UserController.php
+// ========================
+
+
+<?php
+namespace OCA\FileListFinder\Controller;
+
+
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\ILogger;
+use OCP\IRequest;
+
+
+class UserController extends Controller {
+private ILogger $logger;
+
+
+public function __construct($AppName, IRequest $request, ILogger $logger) {
+parent::__construct($AppName, $request);
+$this->logger = $logger;
+}
+
+
+public function index(): TemplateResponse {
+return new TemplateResponse('filelistfinder', 'user');
+}
+
+
+public function submit(): TemplateResponse {
+// TODO: handle file or textarea input, enqueue background job
+$this->logger->info("[FileListFinder] Job submitted by user");
+return new TemplateResponse('filelistfinder', 'user', [
+'status' => 'Submitted!'
+]);
+}
+}
