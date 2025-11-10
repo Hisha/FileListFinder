e74a866ed1224fb24cdf7cdc1ab21399da323ac5
Kevin Smith <vndinmchine@gmail.com>
2025-11-10 05:47:58 -0500

    Refactor FileListProcessor to include constructor and logging

--- lib/BackgroundJob/FileListProcessor.php
+++ lib/BackgroundJob/FileListProcessor.php
@@ -1,16 +1,18 @@
 <?php
 namespace OCA\FileListFinder\BackgroundJob;
 
-
 use OCP\BackgroundJob\TimedJob;
 use OCP\ILogger;
 
-
 class FileListProcessor extends TimedJob {
-protected function run($argument): void {
-/** @var ILogger $logger */
-$logger = \OC::$server->getLogger();
-$logger->info("[FileListFinder] Background job triggered");
-// TODO: implement file search + zip logic
-}
+    public function __construct() {
+        // Run at most once every 10 minutes
+        $this->setInterval(600); // 600 seconds = 10 minutes
+    }
+
+    protected function run($argument): void {
+        /** @var ILogger $logger */
+        $logger = \OC::$server->getLogger();
+        $logger->info("[FileListFinder] Background job triggered with argument: " . json_encode($argument));
+    }
 }
