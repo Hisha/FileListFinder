2b13fda30085100fbfacb726c3e2c99a655e561e
Kevin Smith <vndinmchine@gmail.com>
2025-11-10 05:52:18 -0500

    Refactor FileListProcessor to extend Job instead of TimedJob

--- lib/BackgroundJob/FileListProcessor.php
+++ lib/BackgroundJob/FileListProcessor.php
@@ -1,18 +1,9 @@
-<?php
-namespace OCA\FileListFinder\BackgroundJob;
-
-use OCP\BackgroundJob\TimedJob;
+use OCP\BackgroundJob\Job;
 use OCP\ILogger;
 
-class FileListProcessor extends TimedJob {
-    public function __construct() {
-        // Run at most once every 10 minutes
-        $this->setInterval(600); // 600 seconds = 10 minutes
-    }
-
+class FileListProcessor extends Job {
     protected function run($argument): void {
-        /** @var ILogger $logger */
         $logger = \OC::$server->getLogger();
-        $logger->info("[FileListFinder] Background job triggered with argument: " . json_encode($argument));
+        $logger->info('[FileListFinder] Queued job triggered with argument: ' . json_encode($argument));
     }
 }
